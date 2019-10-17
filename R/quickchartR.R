95bcb2950ea5720a3f4ed343d8567131268fce2e
JanSawicki <jansawickisawicki@gmail.com>
2019-10-17 15:27:44 +0200

    Added basic quickchart generation

---
+++ R/quickchartR.R
@@ -0,0 +1,29 @@
+#' @title hello function
+#'
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+require(rjson)
+quickchartR <- function(chartType, data) {
+  MAIN_LINK = "https://quickchart.io/chart?c="
+
+  json <- list(type = type,
+               data = data)
+  json = toJSON(json)
+
+  paste0(MAIN_LINK, json)
+}
+
+type = "bar"
+labels = c('a', 'b', 'c', 'd', 'e')
+
+datasets = list(list(label = "X", data = list(1, 2, 3)), list(label = "Y", data =
+                                                                list(2, 4, 6)))
+data = list(labels = labels, datasets = datasets)
+
+url = quickchartR(type, data)
+
+browseURL(url)
