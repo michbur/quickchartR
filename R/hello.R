259a5e5c43d1813d42d3a175b8ef2cf8a2157fca
JanSawicki <jansawickisawicki@gmail.com>
2019-10-17 14:48:59 +0200

    Added auto-generated R package project

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+#' @title hello function
+#'
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
