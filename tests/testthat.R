e26962c0c63e27b20039376f0324bbffc70cef96
Jacek <mynaj@student.mini.pw.edu.pl>
2019-11-01 16:18:04 +0100

    First tests added.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,20 @@
 library(testthat)
 library(quickchartR)
 
+test_that("output type of getLabels", {
+  labels <- 1:10
+  expect_type(getLabels(labels), "character")
+})
+
+test_that("unique test of getLabels", {
+  labels <- c(1,2,3,3,3,4,5,5)
+  expect_equal(length(getLabels(labels)), 5)
+})
+
+test_that("output type of inputDataToNamedList", {
+  inputData <- data.frame("x" = 1:10, "y" = 1:10, "r" = 1:10)
+  expect_type(inputDataToNamedList(inputData), "list")
+})
+
+
 test_check("quickchartR")
