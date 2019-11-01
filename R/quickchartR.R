5612059ae64cad0ddf3f5be2272aa4bf7bf6d6ec
JanSawicki <jansawickisawicki@gmail.com>
2019-11-01 13:21:57 +0100

    Refactored main functions and split to smaller ones

--- R/quickchartR.R
+++ R/quickchartR.R
@@ -1,5 +1,3 @@
-# Here's a good place to put your top-level package documentation
-
 .onAttach <- function (lib, pkgname = "quickchartR") {
   ## Put stuff here you want to run when your package is loaded
   if (!require(rjson))
@@ -9,10 +7,10 @@
   invisible()
 }
 
-# radialGauges are not included
-#"radialGauge",
-# sparklines are not included because they are a terrible visualization
-#"sparkline"
+# ---------------------------------- CONSTANTS ----------------------------------
+
+# "radialGauge" type is not included
+# "sparkline" type is not included because they are a terrible visualizations
 TYPES = list("bar",
              "line",
              "radar",
@@ -20,6 +18,10 @@ TYPES = list("bar",
              "doughnut",
              "scatter",
              "bubble")
+
+MAIN_LINK = paste0("https://quickchart.io/chart?")
+
+# ---------------------------------- FUNCTIONS ----------------------------------
 
 checkTypes = function(types) {
   for (type in types) {
@@ -32,17 +34,20 @@ checkTypes = function(types) {
   }
 }
 
+# takes a data frame with columns x, y, r
+# returns a list that will be converted to JSON
 inputDataToNamedList = function(dataFrame) {
   print(dataFrame)
   namedList = list()
   for (index in 1:nrow(dataFrame)) {
-    row = dataFrame[index,]
+    row = dataFrame[index, ]
     newList = list(x = row$x, y = row$y, r = row$r)
     namedList[[length(namedList) + 1]] <- newList
   }
   namedList
 }
 
+# prepares datasets in list form to be converted to json
 getDatasets = function(types, inputData, labels, colors) {
   datasets = list()
 
@@ -59,7 +64,7 @@ getDatasets = function(types, inputData, labels, colors) {
       nextDataset = list(
         label = labels[i],
         data =
-          as.list(inputData[inputData$label == labels[i],]$y),
+          as.list(inputData[inputData$label == labels[i], ]$y),
         backgroundColor = colors[i]
       )
     }
@@ -74,7 +79,9 @@ getDatasets = function(types, inputData, labels, colors) {
   datasets
 }
 
-# https://github.com/mini-pw/2020Z-ProgramowanieWR/blob/9f1a3e4364fd372b92ede3635a8ed14f8e3fd430/Prezentacje/P2.Rmd#L112
+# https://github.com/mini-pw/2020Z-ProgramowanieWR/blob/9f1a3e4364fd372b92ede3635a8ed14f8e3fd430/Prezentacje/P2.Rmd#L157
+# gets element (e.g. column) from an object (e.g. dataframe) using NSE, i.e. element_name is not string but object
+# e.g. getElementNse(input_list, x) returns input_list$x (x is NOT a string)
 getElementNse <- function(input_list, element_name) {
   s_element_name <- element_name
   if (as.character(s_element_name) %in% names(input_list)) {
@@ -84,7 +91,30 @@ getElementNse <- function(input_list, element_name) {
   }
 }
 
-#' @title hello function
+getLabels = function(labels) {
+  as.character(unique(labels))
+}
+
+prepareData = function(categories, datasets) {
+  list(labels = categories, datasets = datasets)
+}
+
+prepareJson = function(mainType, data, options) {
+  list(type = mainType,
+       data = data,
+       options = options)
+}
+
+createLink = function(json, base64) {
+  paste0(
+    MAIN_LINK,
+    ifelse(base64, "&encoding=base64", ""),
+    "&c=",
+    ifelse(base64, base64encode(json), json)
+  )
+}
+
+#' @title main function
 #'
 #' @export
 quickchartR <-
@@ -105,36 +135,24 @@ quickchartR <-
     labels = substitute(labels)
     rData = substitute(rData)
 
-
-    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
     inputData$x = getElementNse(inputData, xData)
     inputData$y = getElementNse(inputData, yData)
     inputData$labels = getElementNse(inputData, labels)
-    if (!is.null(rData))
-    {
-      inputData$r = getElementNse(inputData, rData)
-    }
+    #if (!is.null(rData))
+    #{
+    inputData$r = getElementNse(inputData, rData)
+    #}
 
-    MAIN_LINK = paste0("https://quickchart.io/chart?")
-
-    labels = as.character(unique(inputData$labels))
+    labels = getLabels(inputData$labels)
 
     # categories == values of x
     categories = unique(inputData$x)
 
     datasets = getDatasets(types, inputData, labels, colors)
 
-    data = list(labels = categories, datasets = datasets)
+    data = prepareData(categories, datasets)
 
-    json <- list(type = types[1],
-                 data = data,
-                 options = options)
-    json = toJSON(json)
+    json = toJSON(prepareJson(types[1], data, options))
 
-    paste0(
-      MAIN_LINK,
-      ifelse(base64, "&encoding=base64", ""),
-      "&c=",
-      ifelse(base64, base64encode(json), json)
-    )
+    createLink(json, base64)
   }
